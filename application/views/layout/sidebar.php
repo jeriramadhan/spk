d1cbb810edb8c31b60a36389460347bc1172ba61
Jeri Ramadhan <jeriramadhan25@gmail.com>
2020-06-25 09:57:19 +0700

    contoh bikin menu side bar

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -18,6 +18,9 @@
             <li class="header">MAIN NAVIGATION</li>
             <li><a href="<?= site_url('kriteria') ?>"><i class="fa fa-file-pdf-o"></i> <span>Kriteria</span></a></li>
             <li><a href="<?= site_url('mahasiswa') ?>"><i class="fa fa-user"></i> <span>Mahasiswa</span></a></li>
+            <!-- ini fungsi bikin menu -->
+            <li><a href="#"><i class="fa fa-user"></i> <span>Pendaftar</span></a></li>
+             <!-- sampe sini, lu bikin dari tag <li> sampe </li> sesuai sama menu yang lu inginkan  -->
         </ul>
     </section>
     <!-- /.sidebar -->
