70c1336ea5d3e81fd220cdad6f30bf8c7b0b7b56
nurul azizah <nurulazizah2498@gmail.com>
2020-06-30 11:36:50 +0700

    tambah pendaftar

--- application/views/menu/creatependaftar.php
+++ application/views/menu/creatependaftar.php
@@ -21,24 +21,24 @@
                     <div class="col-md-12 row">
                         <div class="form-group col-md-4">
                             <label>Nama</label>
-                            <input type="text" class="form-control" name="nama" placeholder="Enter Nama" required>
+                            <input type="text" class="form-control" name="nama" placeholder="Masukkan nama" required>
                         </div>
                         <!-- ganti sesuai kolom -->
                         <div class="form-group col-md-4">
                             <label>Prodi</label>
-                            <input type="text" class="form-control" name="prodi" placeholder="Enter Prodi" required>
+                            <input type="text" class="form-control" name="prodi" placeholder="Masukkan Prodi" required>
                         </div>
                         <div class="form-group col-md-4">
                             <label>Fakultas</label>
-                            <input type="text" class="form-control" name="fakultas" placeholder="Enter Fakultas" required>
+                            <input type="text" class="form-control" name="fakultas" placeholder="Masukkan Fakultas" required>
                         </div>
                         <div class="form-group col-md-4">
                             <label>No Telp</label>
-                            <input type="text" class="form-control" name="no_hp" placeholder="Enter No Telp" required>
+                            <input type="text" class="form-control" name="no_hp" placeholder="Masukkan No Telp" required>
                         </div>
                         <div class="form-group col-md-4">
                             <label>Email</label>
-                            <input type="text" class="form-control" name="email" placeholder="Enter Email" required>
+                            <input type="text" class="form-control" name="email" placeholder="Masukkan Email" required>
                         </div>
                     </div>
                 </div>
